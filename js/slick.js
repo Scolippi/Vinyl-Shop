9b4c2c24232fca184635fd39f7209d4b9d6cddb7
Kacper Krawiec <krawiecr03@gmail.com>
2022-08-24 23:17:15 +0200

    finished working on bestsellers section

--- js/slick.js
+++ js/slick.js
@@ -1,26 +1,52 @@
+// $('.bestsellers__carousel-area').slick({
+//     centerMode: true,
+//     centerPadding: '60px',
+//     slidesToShow: 2,
+//     autoplay: true,
+//     responsive: [
+//       {
+//         breakpoint: 768,
+//         settings: {
+//           arrows: false,
+//           centerMode: true,
+//           centerPadding: '40px',
+//           slidesToShow: 3
+//         }
+//       },
+//       {
+//         breakpoint: 480,
+//         settings: {
+//           arrows: false,
+//           centerMode: true,
+//           centerPadding: '40px',
+//           slidesToShow: 1
+//         }
+//       }
+//     ]
+//   });
+
 $('.bestsellers__carousel-area').slick({
-    centerMode: true,
-    centerPadding: '60px',
-    slidesToShow: 2,
-    autoplay: true,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 3
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 1
-        }
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true,
+  mobileFirst: true,
+  autoplay: true,
+  arrows: false,
+  responsive: [
+    {
+      breakpoint: 720,
+      settings: {
+        slidesToShow: 2,
       }
-    ]
-  });
+    },
+    {
+      breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+        }
+      
+    }
+  ]
+});
